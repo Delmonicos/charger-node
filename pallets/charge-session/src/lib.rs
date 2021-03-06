bb98f149e26e3ee916a98ac35e52f58a6919e61e
Antoine <antoine.detante@gmail.com>
2021-03-06 09:17:36 +0100

    refacto(pallet): switch to new macro syntax

--- pallets/charge-session/src/lib.rs
+++ pallets/charge-session/src/lib.rs
@@ -1,79 +1,89 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use codec::{Encode, Decode};
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch};
-use frame_system::ensure_signed;
 
 use pallet_timestamp as timestamp;
 
 #[derive(Debug, PartialEq, Default, Encode, Decode)]
-struct ChargeRequest<ChargerId, Moment> {
+pub struct ChargeRequest<ChargerId, Moment> {
   charger_id: ChargerId,
   timestamp: Moment,
 }
 
 #[derive(Debug, PartialEq, Default, Encode, Decode)]
-struct ChargingSession<ChargerId, Moment> {
+pub struct ChargingSession<ChargerId, Moment> {
   charger_id: ChargerId,
   started_at: Moment,
 }
 
-pub trait Config: timestamp::Config {
-  type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
-}
+pub use pallet::*;
 
-decl_storage! {
-  trait Store for Module<T: Config> as ChargeSession {
-    // Map of UserAccountId => Latest ChargeRequest
-    UserRequests: map hasher(blake2_128_concat) T::AccountId => ChargeRequest<T::AccountId, T::Moment>;
-    // Map of UserAccountId => Current ChargingSession
-    PendingSessions: map hasher(blake2_128_concat) T::AccountId => ChargingSession<T::AccountId, T::Moment>;
+#[frame_support::pallet]
+pub mod pallet {
+  use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+	use super::*;
+
+  #[pallet::config]
+  #[pallet::disable_frame_system_supertrait_check]
+  pub trait Config: timestamp::Config {
+    type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
   }
-}
 
-decl_event!(
-  pub enum Event<T> where AccountId = <T as frame_system::Config>::AccountId, Moment = <T as timestamp::Config>::Moment {
+  #[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+
+  #[pallet::storage]
+  pub type UserRequests<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, ChargeRequest<T::AccountId, T::Moment>>;
+
+  #[pallet::storage]
+  pub type PendingSessions<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, ChargingSession<T::AccountId, T::Moment>>;
+
+  #[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+  pub enum Event<T: Config> {
     // SessionStarted(User, Charger, Timestamp)
-    SessionStarted(AccountId, AccountId, Moment),
+    SessionStarted(T::AccountId, T::AccountId, T::Moment),
     // SessionEnded(User, Charger, Timestamp)
-    SessionEnded(AccountId, AccountId, Moment),
+    SessionEnded(T::AccountId, T::AccountId, T::Moment),
   }
-);
 
-decl_error! {
-  pub enum Error for Module<T: Config> {
+  #[pallet::error]
+  pub enum Error<T> {
     NoChargingRequest,
     NoChargingSession,
   }
-}
 
-decl_module! {
-  pub struct Module<T: Config> for enum Call where origin: T::Origin {
-    type Error = Error<T>;
+  #[pallet::hooks]
+  impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
-    fn deposit_event() = default;
-
-    #[weight = 1_000]
-    pub fn new_request(origin, charger: T::AccountId) -> dispatch::DispatchResult {
+  #[pallet::call]
+  impl<T: Config> Pallet<T> {
+    
+    #[pallet::weight(1_000)]
+    pub fn new_request(origin: OriginFor<T>, charger: T::AccountId) -> DispatchResultWithPostInfo {
       let sender = ensure_signed(origin)?;
       let now = <timestamp::Module<T>>::get();
 
       // Add the request to the storage with current timestamp
       UserRequests::<T>::insert(&sender, ChargeRequest{ charger_id: charger, timestamp: now });
 
-      Ok(())
+      Ok(().into())
     }
 
-    #[weight = 1_000]
-    pub fn start_session(origin, user: T::AccountId) -> dispatch::DispatchResult {
+    #[pallet::weight(1_000)]
+    pub fn start_session(origin: OriginFor<T>, user: T::AccountId) -> DispatchResultWithPostInfo {
       let sender = ensure_signed(origin)?;
       let now = <timestamp::Module<T>>::get();
 
       // Validate that a request exists for this user & charger
       let request = UserRequests::<T>::get(&user);
-      if request.charger_id != sender {
-        // Reject the session
-        return Err(Error::<T>::NoChargingRequest.into());
+      match request {
+        None => return Err(Error::<T>::NoChargingRequest.into()),
+        Some(request) if request.charger_id != sender => return Err(Error::<T>::NoChargingRequest.into()),
+        _ => {},
       }
       // TODO: check timestamp for maximal period of time between new_request & start_session ?
       // TODO: check that this user does not have another active charging session
@@ -85,21 +95,22 @@ decl_module! {
       PendingSessions::<T>::insert(&user, ChargingSession{ charger_id: sender.clone(), started_at: now });
 
       // Emit an event
-      Self::deposit_event(RawEvent::SessionStarted(user, sender, now));
+      Self::deposit_event(Event::SessionStarted(user, sender, now));
 
-      Ok(())
+      Ok(().into())
     }
 
-    #[weight = 1_000]
-    pub fn end_session(origin, user: T::AccountId) -> dispatch::DispatchResult {
+    #[pallet::weight(1_000)]
+    pub fn end_session(origin: OriginFor<T>, user: T::AccountId) -> DispatchResultWithPostInfo {
       let sender = ensure_signed(origin)?;
       let now = <timestamp::Module<T>>::get();
 
       // Validate that a session exists for this user & charger
       let session = PendingSessions::<T>::get(&user);
-      if session.charger_id != sender {
-        // Reject the session
-        return Err(Error::<T>::NoChargingSession.into());
+      match session {
+        None => return Err(Error::<T>::NoChargingSession.into()),
+        Some(session) if session.charger_id != sender => return Err(Error::<T>::NoChargingSession.into()),
+        _ => {},
       }
       
       // Remove the request from storage
@@ -108,10 +119,11 @@ decl_module! {
       // TODO: offchain storage of charging time
 
       // Emit an event
-      Self::deposit_event(RawEvent::SessionEnded(user, sender, now));
+      Self::deposit_event(Event::SessionEnded(user, sender, now));
 
-      Ok(())
+      Ok(().into())
     }
 
   }
+
 }
