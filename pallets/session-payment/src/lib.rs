c50d2318d08efa962e645cf5a93666e457d28ea5
Fabrice CROISEAUX <fcroiseaux@gmail.com>
2021-03-06 11:55:27 +0100

    First version of payment pallet

--- pallets/session-payment/src/lib.rs
+++ pallets/session-payment/src/lib.rs
@@ -1,117 +1,109 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use codec::{Encode, Decode};
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch};
-use frame_system::ensure_signed;
 
 use pallet_timestamp as timestamp;
+use sp_std::prelude::*;
+
+
+type Currency = u128;
+type SubsStr = Vec<u8>;
 
 #[derive(Debug, PartialEq, Default, Encode, Decode)]
-struct ChargeRequest<ChargerId, Moment> {
-  charger_id: ChargerId,
-  timestamp: Moment,
+pub struct UserConsent<Moment> {
+	timestamp: Moment,
+	iban: SubsStr,
+	bic_code: SubsStr,
 }
 
 #[derive(Debug, PartialEq, Default, Encode, Decode)]
-struct ChargingSession<ChargerId, Moment> {
-  charger_id: ChargerId,
-  started_at: Moment,
+pub struct PaymentRequest<AccountId, Moment> {
+	account_id: AccountId,
+	timestamp: Moment,
+	amount: Currency
 }
 
-pub trait Config: timestamp::Config {
-  type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
-}
+pub use pallet::*;
 
-decl_storage! {
-  trait Store for Module<T: Config> as ChargeSession {
-    // Map of UserAccountId => Latest ChargeRequest
-    UserRequests: map hasher(blake2_128_concat) T::AccountId => ChargeRequest<T::AccountId, T::Moment>;
-    // Map of UserAccountId => Current ChargingSession
-    PendingSessions: map hasher(blake2_128_concat) T::AccountId => ChargingSession<T::AccountId, T::Moment>;
-  }
-}
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+	use super::*;
 
-decl_event!(
-  pub enum Event<T> where AccountId = <T as frame_system::Config>::AccountId, Moment = <T as timestamp::Config>::Moment {
-    // SessionStarted(User, Charger, Timestamp)
-    SessionStarted(AccountId, AccountId, Moment),
-    // SessionEnded(User, Charger, Timestamp)
-    SessionEnded(AccountId, AccountId, Moment),
-  }
-);
+	#[pallet::config]
+	#[pallet::disable_frame_system_supertrait_check]
+	pub trait Config: timestamp::Config {
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
 
-decl_error! {
-  pub enum Error for Module<T: Config> {
-    NoChargingRequest,
-    NoChargingSession,
-  }
-}
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
 
-decl_module! {
-  pub struct Module<T: Config> for enum Call where origin: T::Origin {
-    type Error = Error<T>;
 
-    fn deposit_event() = default;
+	#[pallet::storage]
+	pub type UserConsents<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, UserConsent<T::Moment>>;
 
-    #[weight = 1_000]
-    pub fn new_request(origin, charger: T::AccountId) -> dispatch::DispatchResult {
-      let sender = ensure_signed(origin)?;
-      let now = <timestamp::Module<T>>::get();
 
-      // Add the request to the storage with current timestamp
-      UserRequests::<T>::insert(&sender, ChargeRequest{ charger_id: charger, timestamp: now });
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		// PaymentProcessed(User, Timestamp, currency)
+		PaymentProcessed(T::AccountId, T::Moment, Currency),
+		// UserConsentAdded(User, Timestamp, IBAN, bic)
+		UserConsentAdded(T::AccountId, T::Moment, SubsStr, SubsStr),
+	}
 
-      Ok(())
-    }
+	#[pallet::error]
+	pub enum Error<T> {
+		NoConsentForPayment,
+		UserConsentRefused,
+		PaymentError
+	}
 
-    #[weight = 1_000]
-    pub fn start_session(origin, user: T::AccountId) -> dispatch::DispatchResult {
-      let sender = ensure_signed(origin)?;
-      let now = <timestamp::Module<T>>::get();
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
-      // Validate that a request exists for this user & charger
-      let request = UserRequests::<T>::get(&user);
-      if request.charger_id != sender {
-        // Reject the session
-        return Err(Error::<T>::NoChargingRequest.into());
-      }
-      // TODO: check timestamp for maximal period of time between new_request & start_session ?
-      // TODO: check that this user does not have another active charging session
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
 
-      // Remove the request from storage
-      UserRequests::<T>::take(&user);
+		#[pallet::weight(1_000)]
+		pub fn new_consent(origin: OriginFor<T>, iban: SubsStr, bic_code: SubsStr) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+			let now = <timestamp::Module<T>>::get();
 
-      // Add the pending charging session
-      PendingSessions::<T>::insert(&user, ChargingSession{ charger_id: sender.clone(), started_at: now });
+			// Add the request to the storage with current timestamp
+			UserConsents::<T>::insert(&sender, UserConsent{timestamp: now, iban: iban.clone(), bic_code: bic_code.clone() });
 
-      // Emit an event
-      Self::deposit_event(RawEvent::SessionStarted(user, sender, now));
+			// Fire event
+			Self::deposit_event(Event::UserConsentAdded(sender, now, iban, bic_code));
 
-      Ok(())
-    }
+			Ok(().into())
+		}
 
-    #[weight = 1_000]
-    pub fn end_session(origin, user: T::AccountId) -> dispatch::DispatchResult {
-      let sender = ensure_signed(origin)?;
-      let now = <timestamp::Module<T>>::get();
+		#[pallet::weight(1_000)]
+		pub fn process_payment(origin: OriginFor<T>, amount: Currency) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+			let now = <timestamp::Module<T>>::get();
 
-      // Validate that a session exists for this user & charger
-      let session = PendingSessions::<T>::get(&user);
-      if session.charger_id != sender {
-        // Reject the session
-        return Err(Error::<T>::NoChargingSession.into());
-      }
-      
-      // Remove the request from storage
-      PendingSessions::<T>::take(&user);
+			// Validate that a request exists for this user & charger
+			let consent = UserConsents::<T>::get(&sender);
+			let (_iban, _bic_code) = match consent {
+				None => return Err(Error::<T>::NoConsentForPayment.into()),
+				Some(consent) => (consent.iban, consent.bic_code),
+			//	_ => {},
+			};
 
-      // TODO: offchain storage of charging time
+			// TODO: Execute payment
 
-      // Emit an event
-      Self::deposit_event(RawEvent::SessionEnded(user, sender, now));
+			// Emit an event
+			Self::deposit_event(Event::PaymentProcessed(sender, now, amount));
 
-      Ok(())
-    }
+			Ok(().into())
+		}
 
-  }
+
+	}
+
 }
