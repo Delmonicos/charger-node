56bdb93453ca8f7bb7a00c3ddbba1a8de48461ac
Fabrice CROISEAUX <fcroiseaux@gmail.com>
2021-03-26 21:54:25 +0100

    Init payment service

---
+++ payment-service/src/runtime/offchain.rs
@@ -0,0 +1,30 @@
+use codec::{Decode, Encode};
+use sp_externalities::ExternalitiesExt;
+use sp_runtime_interface::pass_by::PassByCodec;
+
+#[cfg(feature = "std")]
+use super::externalities::PaymentExt;
+
+#[derive(Encode, Decode, PassByCodec)]
+pub enum ChargeStatus {
+    NoCharge,
+    Active,
+    Ended { kwh: u64 },
+}
+
+#[sp_runtime_interface::runtime_interface]
+pub trait Api {
+    fn start_charge(&mut self) -> bool {
+        return self
+            .extension::<PaymentExt>()
+            .expect("no extension")
+            .start_charge();
+    }
+
+    fn get_current_charge_status(&mut self) -> ChargeStatus {
+        return self
+            .extension::<PaymentExt>()
+            .expect("no extension")
+            .get_current_charge_status();
+    }
+}
