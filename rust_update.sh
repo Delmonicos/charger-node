350cccd31b66a9f9e9b02ca88e9050c35e3dd5ef
Fabrice CROISEAUX <fcroiseaux@gmail.com>
2021-04-03 11:13:03 +0200

    Test update

--- rust_update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt install -y cmake pkg-config libssl-dev git build-essential clang libclang-dev curl libz-dev
-echo 'Updating Rust'
-rustup update
-rustup update nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
-
