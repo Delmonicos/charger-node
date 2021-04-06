a127b4b7c85acf4d7953b43d3299e57df89f5ece
Fabrice CROISEAUX <fcroiseaux@gmail.com>
2021-04-06 09:00:31 +0200

    Add substrate_env_install for Clever Cloud

--- rust_update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo 'Updating Rust'
-rustup update
-rustup update nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
-
---
+++ substrate_env_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y cmake pkg-config libssl-dev git build-essential clang libclang-dev curl libz-dev
+
+
+echo 'Updating Rust'
+rustup default stable
+rustup update
+rustup update nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
+
